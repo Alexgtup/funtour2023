f2dd3fce82a5c6e31324d100c87540134b577f91
Alexgtup <alexgtup@gmail.com>
2023-03-18 22:53:20 +0300

    Добавлены файлы index.html и style.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import {answers} from './answers.js'
+import {questions} from './questions.js'
+import './index.css';
 
-function App() {
+function Site() {
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+  const [typingText, setTypingText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const typingTextRef = useRef(null);
+
+  useEffect(() => {
+    if (selectedQuestion !== null) {
+      const text = answers.find(answer => answer.id === selectedQuestion).text;
+      const interval = setInterval(() => {
+        if (currentIndex < text.length) {
+          setTypingText(prevTypingText => prevTypingText + text[currentIndex]);
+          setCurrentIndex(prevIndex => prevIndex + 1);
+          typingTextRef.current.scrollLeft = typingTextRef.current.scrollWidth;
+        } else {
+          clearInterval(interval);
+        }
+      }, 50);
+
+      return () => clearInterval(interval);
+    }
+  }, [selectedQuestion, currentIndex]);
+
+  function handleQuestionClick(questionId) {
+    setSelectedQuestion(questionId);
+    setTypingText("");
+    setCurrentIndex(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="site">
+      <div className="questions">
+        {questions.map((question) => (
+          <div key={question.id} className={`question ${selectedQuestion === question.id ? 'selected' : ''}`} onClick={() => handleQuestionClick(question.id)}>
+            {question.text}
+          </div>
+        ))}
+      </div>
+      <div className="answers">
+        {selectedQuestion && (
+          <>
+            <div className="typing-text" ref={typingTextRef}>{typingText}<div className="cursor"></div></div>
+
+          </>
+        )}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Site;
